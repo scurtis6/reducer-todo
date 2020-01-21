7ef7d82142a396aa74222a592460103e1d5bbd67
Sierra Curtis <sierracurtis91@gmail.com>
2020-01-21 16:57:18 -0500

    added reducer and exported it

---
+++ my-app/src/reducers/reducer.js
@@ -0,0 +1,11 @@
+import React, { useState, useReducer } from 'react';
+
+export const initialState = {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 3892987589
+};
+
+export const reducer = () => {
+    return
+}
